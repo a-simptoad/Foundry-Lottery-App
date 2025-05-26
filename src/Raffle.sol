4ffdc063d73caf176ec887bf2855c57323a9c6fa
root <root@LAPTOP-SKKAKEFM.>
2025-05-26 08:35:57 +0000

    feat: Added timestamp for automatic functioning of pickWinner()

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -26,19 +26,24 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-error NotEnoughEth();
+error Raffle_NotEnoughEth();
+error Raffle_TimeNotPassed();
 
 contract Raffle {
     uint256 immutable i_entraceFee;
     address payable[] private s_players;
+    uint256 private immutable i_interval;
+    uint256 private immutable s_lastTimeStamp;
 
     event EnteredRaffle(address indexed player);
 
-    constructor(uint256 entranceFee) {
+    constructor(uint256 entranceFee, uint256 interval) {
         i_entraceFee = entranceFee;
+        i_interval = interval;
+        s_lastTimeStamp = block.timestamp;
     }
 
-    function enterRaffle() public payable{
+    function enterRaffle() external payable{
 
         // NOT VERY GAS EFFICIENT
         // require(msg.value >= i_entraceFee, "Not enough ETH sent");  //LEAST GAS EFFICIENT DUE TO STRINGS
@@ -46,14 +51,23 @@ contract Raffle {
 
         // MOST GAS EFFICIENT
         if (msg.value < i_entraceFee) {
-            revert NotEnoughEth();
+            revert Raffle_NotEnoughEth();
         }
 
         s_players.push(payable(msg.sender));
         emit EnteredRaffle(msg.sender);
     }
 
-    function pickWinner() public{
+    // Get a random number 
+    // Use a random number to pick a player
+    // Be automatically called 
+    function pickWinner() external {
+
+        // Check if enough time has passed
+        if(block.timestamp - s_lastTimeStamp > i_interval){
+            revert Raffle_TimeNotPassed();
+        }
+
 
     }
 }
