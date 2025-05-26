06409c8289e9833fe86af702241f1feb4d651855
root <root@LAPTOP-SKKAKEFM.>
2025-05-26 18:15:09 +0000

    feat: Added a request to chainlink oracle for random number generation

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -26,25 +26,42 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
+import {VRFConsumerBaseV2Plus} from "@chainlink/contracts/src/v0.8/vrf/dev/VRFConsumerBaseV2Plus.sol";
+import {VRFV2PlusClient} from "@chainlink/contracts/src/v0.8/dev/vrf/libraries/VRFV2PlusClient.sol";
+
 error Raffle_NotEnoughEth();
 error Raffle_TimeNotPassed();
 
-contract Raffle {
-    uint256 immutable i_entraceFee;
-    address payable[] private s_players;
+contract Raffle is VRFConsumerBaseV2Plus {
+    uint16 private constant REQUEST_CONFIRMATIONS = 3;
+    uint32 private constant NUM_WORDS = 1;
+    uint256 private immutable i_entraceFee;
     uint256 private immutable i_interval;
-    uint256 private immutable s_lastTimeStamp;
+    bytes32 private immutable i_keyHash;
+    uint256 private immutable i_subscriptionId;
+    uint32 private immutable i_callbackGasLimit;
+    address payable[] private s_players;
+    uint256 private s_lastTimeStamp;
 
     event EnteredRaffle(address indexed player);
 
-    constructor(uint256 entranceFee, uint256 interval) {
+    constructor(
+        uint256 entranceFee,
+        uint256 interval,
+        address vrfCoordinator,
+        bytes32 keyHash,
+        uint256 subscriptionId,
+        uint32 callbackGasLimit
+    ) VRFConsumerBaseV2Plus(vrfCoordinator) {
         i_entraceFee = entranceFee;
         i_interval = interval;
         s_lastTimeStamp = block.timestamp;
+        i_keyHash = keyHash;
+        i_subscriptionId = subscriptionId;
+        i_callbackGasLimit = callbackGasLimit;
     }
 
-    function enterRaffle() external payable{
-
+    function enterRaffle() external payable {
         // NOT VERY GAS EFFICIENT
         // require(msg.value >= i_entraceFee, "Not enough ETH sent");  //LEAST GAS EFFICIENT DUE TO STRINGS
         // require(msg.value >= i_entraceFee, NotEnoughEth());   //REVERTS WITH CUSTOM ERROR, MORE GAS EFFICIENT
@@ -58,16 +75,30 @@ contract Raffle {
         emit EnteredRaffle(msg.sender);
     }
 
-    // Get a random number 
+    // Get a random number
     // Use a random number to pick a player
-    // Be automatically called 
+    // Be automatically called
     function pickWinner() external {
-
         // Check if enough time has passed
-        if(block.timestamp - s_lastTimeStamp > i_interval){
+        if (block.timestamp - s_lastTimeStamp > i_interval) {
             revert Raffle_TimeNotPassed();
         }
 
+        // s_vrfCoordinator is a coordinator which requests random number from the Chainlink oracle.
+        VRFV2PlusClient.RandomWordsRequest memory request = VRFV2PlusClient.RandomWordsRequest({
+            keyHash: i_keyHash,
+            subId: i_subscriptionId,
+            requestConfirmations: REQUEST_CONFIRMATIONS,
+            callbackGasLimit: i_callbackGasLimit,
+            numWords: NUM_WORDS,
+            extraArgs: VRFV2PlusClient._argsToBytes(
+                // Set nativePayment to true to pay for VRF requests with Sepolia ETH instead of LINK
+                VRFV2PlusClient.ExtraArgsV1({nativePayment: false})
+            )
+        });
 
+        uint256 requestId = s_vrfCoordinator.requestRandomWords(request);
     }
+    
+    function fulfillRandomWords(uint256 requestId, uint256[] memory randomWords) internal override {}
 }
